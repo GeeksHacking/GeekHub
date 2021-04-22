6e0555a91171b3db66cb8a57578d0ddb99cef958
Guan Qin <qinguan20040914@outlook.com>
2021-04-22 15:09:56 +0800

    Initial commit

---
+++ Data/ApplicationApiAuthorizationDbContext.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Threading.Tasks;
+using IdentityServer4.EntityFramework.Entities;
+using IdentityServer4.EntityFramework.Extensions;
+using IdentityServer4.EntityFramework.Interfaces;
+using IdentityServer4.EntityFramework.Options;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace GeekHub.Data
+{
+    /// <summary>
+    /// Database abstraction for a combined <see cref="DbContext"/> using ASP.NET Identity and Identity Server.
+    /// </summary>
+    /// <typeparam name="TUser"></typeparam>
+    /// <typeparam name="TRole"></typeparam>
+    /// <typeparam name="TKey">Key of the IdentityUser entity</typeparam>
+    public class ApplicationApiAuthorizationDbContext<TUser, TRole, TKey> : IdentityDbContext<TUser, TRole, TKey>, IPersistedGrantDbContext
+        where TUser : IdentityUser<TKey>
+        where TRole : IdentityRole<TKey>
+        where TKey : IEquatable<TKey>
+    {
+        private readonly IOptions<OperationalStoreOptions> _operationalStoreOptions;
+
+        /// <summary>
+        /// Initializes a new instance of <see cref="StrifeApiAuthorizationDbContext{TUser, TRole, TKey}"/>.
+        /// </summary>
+        /// <param name="options">The <see cref="DbContextOptions"/>.</param>
+        /// <param name="operationalStoreOptions">The <see cref="IOptions{OperationalStoreOptions}"/>.</param>
+        public ApplicationApiAuthorizationDbContext(
+            DbContextOptions options,
+            IOptions<OperationalStoreOptions> operationalStoreOptions)
+            : base(options)
+        {
+            _operationalStoreOptions = operationalStoreOptions;
+        }
+
+        /// <summary>
+        /// Gets or sets the <see cref="DbSet{PersistedGrant}"/>.
+        /// </summary>
+        public DbSet<PersistedGrant> PersistedGrants { get; set; }
+
+        /// <summary>
+        /// Gets or sets the <see cref="DbSet{DeviceFlowCodes}"/>.
+        /// </summary>
+        public DbSet<DeviceFlowCodes> DeviceFlowCodes { get; set; }
+
+        Task<int> IPersistedGrantDbContext.SaveChangesAsync() => base.SaveChangesAsync();
+
+        /// <inheritdoc />
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.ConfigurePersistedGrantContext(_operationalStoreOptions.Value);
+        }
+    }
+}
---
+++ Models/ApplicationRole.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Microsoft.AspNetCore.Identity;
+
+namespace GeekHub.Models
+{
+    public class ApplicationRole: IdentityRole<Guid>
+    {
+        
+    }
+}
